c8a00a9797c6c62b4dcf653459eccdc6b8fbd679
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-09-25 11:13:25 +0200

    more tests

--- tests/group.test.js
+++ tests/group.test.js
@@ -29,32 +29,31 @@ const newGroupName = 'Better named group';
 // });
 
 // GROUP TEST ROUND 2 (NEEDS GroupId from round 1 in context files)
-// test.only('Invite a user to the group', async () => {
-//     const event = eventContext({
-//         pathParameters: { id: testGroupId },
-//         body: {
-//             toName: 'Michiel gast',
-//             toEmail: 'wintvelt@me.com',
-//             message: 'test message',
-//             role: 'guest'
-//         }
-//     });
-//     const response = await sendInvite(event);
-//     expect(response.statusCode).toEqual(200);
-// }, TIMEOUT * 4);
-// test.only('Invite a email to the group', async () => {
-//     const event = eventContext({
-//         pathParameters: { id: testGroupId },
-//         body: {
-//             toName: 'Bas email',
-//             toEmail: 'wouter.intvelt@gmail.com',
-//             message: 'test message',
-//             role: 'admin'
-//         }
-//     });
-//     const response = await sendInvite(event);
-//     expect(response.statusCode).toEqual(200);
-// }, TIMEOUT * 4);
+test.only('Invite user and email to the group', async () => {
+    const event = eventContext({
+        pathParameters: { id: testGroupId },
+        body: {
+            toName: 'Michiel gast',
+            toEmail: 'wintvelt@me.com',
+            message: 'test message',
+            role: 'guest'
+        }
+    });
+    const response = await sendInvite(event);
+    expect(response.statusCode).toEqual(200);
+
+    const event2 = eventContext({
+        pathParameters: { id: testGroupId },
+        body: {
+            toName: 'Bas email',
+            toEmail: 'wouter.intvelt@gmail.com',
+            message: 'test message',
+            role: 'admin'
+        }
+    });
+    const response2 = await sendInvite(event);
+    expect(response2.statusCode).toEqual(200);
+}, TIMEOUT * 4);
 
 test('Change group name', async () => {
     const event = eventContext({
