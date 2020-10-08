1a39892c71ac44353328c1502e6a2ace730dd13c
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-10-08 19:41:01 +0200

    improve output (consistent with memberships)

--- handlersGroup/getGroup.js
+++ handlersGroup/getGroup.js
@@ -1,5 +1,6 @@
 import { handler, getUserFromEvent } from "blob-common/core/handler";
 import { dynamoDb } from "blob-common/core/db";
+import { now } from "blob-common/core/date";
 
 export const main = handler(async (event, context) => {
     const userId = getUserFromEvent(event);
@@ -16,6 +17,16 @@ export const main = handler(async (event, context) => {
     if (!membership || membership.status === 'invite') throw new Error('not a member of this group');
     const userRole = membership.role;
     const isFounder = membership.isFounder;
+    const today = now();
+    const newPicsCount = (membership.seenPics) ?
+        membership.seenPics.filter(item => (!item.seenDate || item.seenDate === today)).length
+        : 0;
 
-    return { ...membership.group, userRole, isFounder };
+    return {
+        ...membership.group,
+        userRole,
+        isFounder,
+        newPicsCount,
+        createdAt: item.createdAt,
+    };
 });
