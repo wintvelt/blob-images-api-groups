435d1d3ca5fa90f726649af7dc37a7afcb568adc
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-10-10 15:38:38 +0200

    fix role check

--- handlersGroup/listMembers.js
+++ handlersGroup/listMembers.js
@@ -26,13 +26,15 @@ export const main = handler(async (event, context) => {
     const membership = await getMember(userId, groupId);
     const groupRole = membership && (membership.status !== 'invite') && membership.role;
     if (!groupRole) throw new Error('no access to group');
-    const isAdmin = (membership.userRole === 'admin');
+    const isAdmin = (groupRole === 'admin');
 
     const members = await getMembersAndInvites(groupId);
-    const hasOtherAdmin = members.find(mem => (mem.user.SK !== userId && mem.userRole === 'admin'));
+    const hasOtherAdmin = members.find(mem => (mem.user.SK !== userId && mem.role === 'admin'));
 
     return members.sort(compareMembers).map(item => ({
         ...item.user,
+        PK: item.PK,
+        SK: item.SK,
         userRole: item.role,
         isFounder: item.isFounder,
         isCurrent: (item.user.SK === userId),
@@ -41,7 +43,7 @@ export const main = handler(async (event, context) => {
             (hasOtherAdmin) ? ['leave'] : []
             : (isAdmin && !item.isFounder) ?
                 [
-                    (item.userRole === 'admin') ? 'guestify' : 'adminify',
+                    (item.role === 'admin') ? 'guestify' : 'adminify',
                     (item.status === 'invite') ? 'uninvite' : 'ban'
                 ]
                 : [],
