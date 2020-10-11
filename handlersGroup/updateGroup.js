03643221b77336030d0f30c0808e2820cc989877
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-10-11 19:07:28 +0200

    return also member attributes in groupUpdate

--- handlersGroup/updateGroup.js
+++ handlersGroup/updateGroup.js
@@ -2,15 +2,16 @@ import { handler, getUserFromEvent } from "blob-common/core/handler";
 import { dbUpdateMulti } from "blob-common/core/db";
 import { sanitize } from "blob-common/core/sanitize";
 import { cleanRecord } from "blob-common/core/dbClean";
-import { getMemberRole, getPhotoById, getPhotoByUrl } from "../libs/dynamodb-lib-single";
+import { getMember, getPhotoById, getPhotoByUrl } from "../libs/dynamodb-lib-single";
+import { now } from "blob-common/core/date";
 
 export const main = handler(async (event, context) => {
     const userId = getUserFromEvent(event);
     const groupId = event.pathParameters.id;
     const data = JSON.parse(event.body);
 
-    const memberRole = await getMemberRole(userId, groupId);
-    if (memberRole !== 'admin') throw new Error('group update not allowed');
+    const membership = await getMember(userId, groupId);
+    if (membership.role !== 'admin' || membership.status === 'invite') throw new Error('group update not allowed');
 
     let groupUpdate = {};
     if (data.name) groupUpdate.name = sanitize(data.name);
@@ -36,8 +37,27 @@ export const main = handler(async (event, context) => {
         };
     }
 
-    if (Object.keys(groupUpdate).length === 0) return 'ok';
+    const userRole = membership.role;
+    const isFounder = membership.isFounder;
+    const today = now();
+    const newPicsCount = (membership.seenPics) ?
+        membership.seenPics.filter(item => (!item.seenDate || item.seenDate === today)).length
+        : 0;
+
+    const newGroup = {
+        ...membership.group,
+        userRole,
+        isFounder,
+        newPicsCount,
+        createdAt: membership.createdAt
+    };
+
+    if (Object.keys(groupUpdate).length === 0) return newGroup;
+
     const result = await dbUpdateMulti('GBbase', groupId, groupUpdate);
 
-    return cleanRecord(result.Attributes);
+    return cleanRecord(
+        ...newGroup,
+        ...result.Attributes
+    );
 });
